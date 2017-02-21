577547821fdcd9051c949ce1ae4fe865c71eb832
Maarten Waanders (Datacon) <mw@datacon.nl>
2017-02-21 17:21:36 +0100

    initial commit

---
+++ src/data/quotes.ts
@@ -0,0 +1,50 @@
+export default [
+ {
+     category: 'inspirational',
+     quotes: [
+        {
+            id: '1',
+            person: 'Bob de Bouwer',
+            text: 'Kunnen wij het maken? Nou en of!'
+        },
+        {
+            id: '2',
+            person: 'Shampoo',
+            text: 'Je bent een ongelofelijke shampoo!'
+        }
+     ],
+     icon: 'brush'
+ },
+ {
+ category: 'ability',
+     quotes: [
+        {
+            id: '4',
+            person: 'Bob de Bouwer',
+            text: 'Kunnen wij het maken? Nou en of!'
+        },
+        {
+            id: '5',
+            person: 'Shampoo',
+            text: 'Je bent een ongelofelijke shampoo!'
+        }
+     ],
+     icon: 'bicycle'
+},
+{
+    category: 'enthusiasm',
+     quotes: [
+        {
+            id: '6',
+            person: 'Bob de Bouwer',
+            text: 'Kunnen wij het maken? Nou en of!'
+        },
+        {
+            id: '7',
+            person: 'Shampoo',
+            text: 'Je bent een ongelofelijke shampoo!'
+        }
+     ],
+     icon: 'battery-charging'
+}
+]
---
+++ src/pages/library/library.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Quote } from "../../data/quote.interface";
+import quotes from '../../data/quotes'; 
+
+
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html'
+})
+export class LibraryPage implements OnInit {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+
+  ngOnInit() {
+    this.quoteCollection = quotes; 
+  }
+}
